6bf83a34438af395281f048e7f1e193fd07c219a
Supreet Kaur <supreet97@gmail.com>
2023-10-05 11:58:47 -0700

    added environment variables

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+
 const questionsRouter = require("./routes/questionsRouter");
+const port = process.env.PORT;
+const api_URL = process.env.API_URL;
 
 app.use(cors());
 app.use(express.static("public"));
